73b2e2504ca1434bf47e190bd4eed337aa514e6d
Rafi Rachmawan <rafirachmawan1987@gmail.com>
2026-04-01 06:30:49 +0700

    Update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.j89iljh2s5g"
+    "revision": "0.90e8rqoq27"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
